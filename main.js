ffbe6cfbe3d32c033151fbeb1b9ff00a1c527a18
eliiiifant <83713264+eliiiifant@users.noreply.github.com>
2021-06-13 19:15:57 +0200

    Add files via upload
    changes

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 function change() {
-    var img1 = "file:///Users/eleonora/Desktop/OneDrive%20-%20momentum/%E2%80%93%E2%80%93%20ZHdK/2.%20Semester/zhdk_bitsandatoms/zhdk_charte_eli/zhdk_charte_eli/assets/Monster_1.svg",
-        img2 = "./assets/Monster_2.svg";
+    var img1 = "./assets/Monster_2.svg",
+        img2 = "./assets/Monster_1.svg";
 
     var imgElement = document.getElementById('img');
-    console.log(imgElement.src);
-    imgElement.src = (imgElement.src === img1)? img2 : img1;
- }
+    var images = document.querySelectorAll('.monster img');
+    console.log('images: ', images);
+
+    images.forEach(image => {
+        image.classList.toggle('is-hidden');
+    })
+    document.getElementById('eyeDiv').classList.toggle('is-hidden');
+}
+
+
+$(".card").mousemove(function(event) {
+    var eye = $(".eye");
+    var x = (eye.offset().left) + (eye.width() / 2);
+    var y = (eye.offset().top) + (eye.height() / 2);
+    var rad = Math.atan2(event.pageX - x, event.pageY - y);
+    var rot = (rad * (180 / Math.PI) * -1) + 180;
+    eye.css({
+      '-webkit-transform': 'rotate(' + rot + 'deg)',
+      '-moz-transform': 'rotate(' + rot + 'deg)',
+      '-ms-transform': 'rotate(' + rot + 'deg)',
+      'transform': 'rotate(' + rot + 'deg)'
+    });
+  });
